40cd5da714bbb6556baa0609abda637047c39882
BigMoistLochu <krok.konrad1@gmail.com>
2024-09-03 19:50:14 +0200

    added game board rendering, now game will render all object 60 per sec

--- src/main/java/aplikacja/gameBoard/GameBoard.java
+++ src/main/java/aplikacja/gameBoard/GameBoard.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class GameBoard extends JPanel {
+public class GameBoard extends JPanel implements Runnable{
 
     //Game Settings
     private final int ORGINAL_TILE_SIZE = 16; //obrazki tworzymy 16x16
@@ -19,13 +19,19 @@ public class GameBoard extends JPanel {
     private final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN; //48x16 = 768 pixeli
     private final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 48x12 = 576 pixeli
 
-    private final Player player = new Player(48,48);
+    private final int FPS = 60; //renderowanie(repaint()) bedzie wykonywal sie 60 razy na sekunde
 
+    private final Player player = new Player(48,48);
+    private Thread gameBoardLoopThread;
     public GameBoard(){
         //Main Setting Board
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+
+        //Create Game Loop Thread
+        this.gameBoardLoopThread = new Thread(this);
+        gameBoardLoopThread.start();
 
         //Setting KeyListener
         KeyMovementListener keyMovementListener = new KeyMovementListener(player);
@@ -33,13 +39,53 @@ public class GameBoard extends JPanel {
 
 
         this.setFocusable(true);
+
     }
+
+    /**
+     * Watek ktory ma za zadanie renderowac obraz gdy obiekty beda zmieniac swoje parametry np gracz czy npc
+     */
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS; // 1 second : FPS = 0.0166( co tyle bedzie sie wykonywalo renderowanie)
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameBoardLoopThread != null){
+
+            currentTime = System.nanoTime();
+
+            delta = delta + (currentTime-lastTime) / drawInterval;
+            timer = timer + (currentTime-lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                repaint();
+                delta = delta - 1;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS: "+ drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        paintPlayer(g);//teraz bedziemy aktualizowac gracza i jego pozycje
-        repaint();
+        updateGame(g);
+    }
+
+    public void updateGame(Graphics g){
+        paintPlayer(g);
+        paintAnotherObjectPlayer(g);
     }
 
     protected void paintPlayer(Graphics graphics){
@@ -47,6 +93,13 @@ public class GameBoard extends JPanel {
         graphics2D.setColor(Color.WHITE);
         graphics2D.fillRect(player.getX(), player.getY(), TILE_SIZE,TILE_SIZE);
     }
+
+    protected void paintAnotherObjectPlayer(Graphics graphics){
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(150, 150, TILE_SIZE,TILE_SIZE);
+    }
+
 
 
 }
